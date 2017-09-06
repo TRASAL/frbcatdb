6a9fe330ec2f1bb7812290fe0bfcfc22fc49f9a4
Ronald van Haren <r.vanharen@esciencecenter.nl>
2017-09-06 11:23:20 +0200

    add some number checking

--- tests/end_to_end_test.py
+++ tests/end_to_end_test.py
@@ -81,11 +81,19 @@ class end2endtest(unittest.TestCase):
         # assert rmp increased by 1
         self.assertEqual(len_before[4], len_after[4]-1)
         # check inserted values in database
-        sql = "select authors.ivorn, authors.contact_name, authors.contact_email, frbs.name, frbs.utc, o.telescope, o.detected, o.verified from radio_measured_params rmp join radio_observations_params rop ON rmp.rop_id=rop.id join observations o on rop.obs_id=o.id join frbs on o.frb_id=frbs.id join authors on frbs.author_id=authors.id where voevent_ivorn='ivo://au.csiro.parkes/parkes#FRB1405141714/57953.44444444';"
+        sql = "select authors.ivorn, authors.contact_name, authors.contact_email, frbs.name, frbs.utc, o.telescope, o.detected, o.verified, rop.beam_semi_major_axis, rop.beam_semi_minor_axis,
+        rop.beam_rotation_angle, rop.sampling_time, rop.bandwidth, rop.centre_frequency,
+        rop.npol, rop.bits_per_sample, rop.gain, rop.tsys, rop.backend, rop.beam,
+        rop.gl, rop.gb, rop.mw_dm_limit,
+        rmp.dm, rmp.dm_error, rmp.width, rmp.snr, rmp.flux, rmp.redshift_inferred
+        from radio_measured_params rmp join radio_observations_params rop ON rmp.rop_id=rop.id join observations o on rop.obs_id=o.id join frbs on o.frb_id=frbs.id join authors on frbs.author_id=authors.id where voevent_ivorn='ivo://au.csiro.parkes/parkes#FRB1405141714/57953.44444444';"
         values = ('ivo://au.csiro.parkes.superb', 'Emily Petroff',
                   'ebpetroff@gmail.com', 'FRB140514',
                   datetime.datetime(2014, 5, 14, 17, 14, 11), 'PARKES',
-                  True, True)  # both detected and verified (importance=1) = True
+                  True, True,
+                  7.5, 7.5, 0, 0.064, 400, 1382, 2, 2, 0.735, 28, 'BPSR', 1,
+                  50.8, -54.6, 34.9,
+                  563.3, 1.0, 4.096, 16.1, 0.361, 0.44)  # both detected and verified (importance=1) = True
         self.cursor.execute(sql)
         self.assertTupleEqual(values, self.cursor.fetchone())
 
@@ -240,13 +248,30 @@ class end2endtest(unittest.TestCase):
         self.cursor.execute(sql)
         self.assertTupleEqual(values, self.cursor.fetchone())
 
+'''
     def test_05(self):
+        '''
+        Validation of addition of notes to the database
+        '''
+        len_before = self.get_num_rows_main_tables()
+        decode.decode_VOEvent(os.path.join(self.test_data, 'Notes_unitTest1.txt'),
+                              self.DB_NAME, self.DB_HOST, self.DB_PORT,
+                              self.USER_NAME, self.USER_PASSWORD, self.LOG_FILE)
+        # check inserted values in database
+        sql = "select frbs.name, o.detected, o.verified from radio_measured_params_notes rmp_notes join radio_measured_params rmp on rmp_notes.rmp_id=rmp.id join radio_observations_params rop ON rmp.rop_id=rop.id join observations o on rop.obs_id=o.id join frbs on o.frb_id=frbs.id join authors on frbs.author_id=authors.id where voevent_ivorn='ivo://nl.astron.apertif/alert#FRB1707201312/57954.55000000';"
+        # both detected and verified should be False for a retracted event
+        values = ('FRB170720', False, False)
+        self.cursor.execute(sql)
+        self.assertTupleEqual(values, self.cursor.fetchone())
+'''
+    def test_06(self):
         '''
         Creating VOEvent from database
         '''
         create.create_VOEvent([1], self.DB_NAME, self.DB_HOST, self.DB_PORT,
                               self.USER_NAME, self.USER_PASSWORD)
 
+Notes_unitTest1.txt
 
 if __name__ == '__main__':
     unittest.main()
